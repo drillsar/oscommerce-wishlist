7c2a053239f06251a68dfb6e4f0e61c1c3849a98
drillsar <iridude@aol.com>
2020-03-01 19:10:42 -0500

    Add Hook process for wishlist

---
+++ catalog/includes/hooks/shop/checkout_process/wishlist.php
@@ -0,0 +1,37 @@
+<?php
+/*
+  $Id: wishlist_hooks.php
+  $Loc: catalog/includes/hooks/shop/wishlist/
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Wishlist v3 for Phoenix 1.0.5.0
+   by @tmccaff 
+
+  Copyright (c) 2018 Thomas McCaffery
+
+  Released under the GNU General Public License
+*/
+
+class hook_shop_checkout_process_wishlist {
+
+  function listen_WishListMod() {
+    global $wishList;
+
+        if (!tep_session_is_registered('wishList') || !is_object($wishList)) {
+                tep_session_register('wishList');
+                $wishList = new wishlist;
+        }
+
+
+  if (isset($_POST['wishlist'])) {
+          if (isset($_POST['products_id']) && is_numeric($_POST['products_id'])) {
+      $attributes = isset($_POST['id']) ? $_POST['id'] : '';
+      $wishList->add_wishlist($_POST['products_id'], $wishList->get_quantity(tep_get_uprid($_POST['products_id'], $attributes))+1, $attributes);
+          }
+                if (WISHLIST_REDIRECT ==  'No') tep_redirect(tep_href_link('product_info.php', 'products_id=' . $_POST['products_id']));
+          tep_redirect(tep_href_link('wishlist.php'));
+  }
+
+
