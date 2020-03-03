ffed0034bcb7a3b921a56dea4bc3b4ac3a2428c0
drillsar <iridude@aol.com>
2020-03-03 08:45:07 -0500

    account and navbar language files

---
+++ catalog/includes/languages/english/modules/content/account/cm_account_wishlist.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+
+  define('MODULE_CONTENT_ACCOUNT_WISHLIST_TITLE', 'My Wishlist');
+  define('MODULE_CONTENT_ACCOUNT_WISHLIST_PUBLIC_TITLE', 'Wishlist');
+  define('MODULE_CONTENT_ACCOUNT_WISHLIST_DESCRIPTION', 'Shows Wishlist link on account.php page');
+  define('MY_WISHLIST_VIEW','View my wishlist');
---
+++ catalog/includes/languages/english/modules/navbar_modules/nb_wishlist.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions 
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_NAVBAR_WISHLIST_TITLE', 'Wishlist');
+  define('MODULE_NAVBAR_WISHLIST_DESCRIPTION', 'Show Wishlist link in Navbar.');
+  
+  define('MODULE_NAVBAR_WISHLIST_PUBLIC_TEXT', '<li><a href="' . tep_href_link('wishlist.php') . '"><i class="fas fa-heart"></i> ' . HEADER_ACCOUNT_WISHLIST . '</a></li>');
+  
