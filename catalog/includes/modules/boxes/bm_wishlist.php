eede6ef860cbc12574d216d1da9af12bf494aa0e
drillsar <iridude@aol.com>
2020-03-03 08:57:53 -0500

    clean up wishlist module box

--- catalog/includes/modules/boxes/bm_wishlist.php
+++ catalog/includes/modules/boxes/bm_wishlist.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: Wishlist Box revision 4 for Phoenix
+  $Id: Wish List Box revision 3
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
@@ -28,27 +28,27 @@ class bm_wishlist {
       }
     }
 
-  function execute() {
+    function execute() {
       global $wishList, $currencies, $languages_id, $oscTemplate;
 
       $counter = $wishList->count_contents();
-        if ($counter > 0) {
+      if ($counter > 0) {
                     $wishlist_box = '';
-            $products = $wishList->get_products();
+        $products = $wishList->get_products();
                                 $n=sizeof($products);
                     if ($n <= MAX_DISPLAY_WISHLIST_BOX) {
-                                for ($i=0; $i<$n; $i++) {
+          for ($i=0; $i<$n; $i++) {
             $wishlist_box .= '<a class="list-group-item list-group-item-action';
-                                        $wishlist_box .= '" href="' . tep_href_link('product_info.php', 'products_id=' . $products[$i]['id']) . '">';
-                                        $wishlist_box .= $products[$i]['quantity'] . ' x ' . $products[$i]['name'];
-                                        $wishlist_box .= '<a href="' . tep_href_link('product_info.php', 'products_id=' . $products[$i]['id']) . '">';
-                                        $wishlist_box .= '</a>';
-                                }
-              $wishlist_totalised = sprintf(MODULE_BOXES_WISHLIST_BOX_CART_TOTAL, $currencies->format($wishList->show_total()));
+            $wishlist_box .= '" href="' . tep_href_link('product_info.php', 'products_id=' . $products[$i]['id']) . '">';
+            $wishlist_box .= $products[$i]['quantity'] . ' x ' . $products[$i]['name'];
+            $wishlist_box .= '</a>';
+          }
+
+          $wishlist_totalized .= sprintf(MODULE_BOXES_WISHLIST_BOX_CART_TOTAL, $currencies->format($wishList->show_total()));
                     }
                 $wishlist_box .= '<a href="' . tep_href_link('wishlist.php') . '">' . sprintf(TEXT_WISHLIST_COUNT, $counter) . '</a>';
             } else {
-                $wishlist_box = '<span class="list-group-item">' . MODULE_BOXES_WISHLIST_BOX_CART_EMPTY . '</span>';
+                    $wishlist_box = '<p>' . MODULE_BOXES_WISHLIST_BOX_CART_EMPTY . '</p>';
             }
 
       $tpl_data = ['group' => $this->group, 'file' => __FILE__];
@@ -64,8 +64,7 @@ class bm_wishlist {
     }
 
     function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Wishlist Box Module', 'MODULE_BOXES_WISHLIST_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('Max Wishlist Box', 'MAX_DISPLAY_WISHLIST_BOX', '4', 'How many wishlist items to display in the infobox before it changes to a counter', '6', '3', now(), now(), NULL, NULL)");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Wish List Box Module', 'MODULE_BOXES_WISHLIST_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_WISHLIST_CONTENT_PLACEMENT', 'Right Column', 'Should the module be loaded in the left or right column?', '6', '1', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_WISHLIST_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
@@ -75,8 +74,8 @@ class bm_wishlist {
     }
 
     function keys() {
-      return array('MODULE_BOXES_WISHLIST_STATUS', 'MAX_DISPLAY_WISHLIST_BOX', 'MODULE_BOXES_WISHLIST_CONTENT_PLACEMENT', 'MODULE_BOXES_WISHLIST_SORT_ORDER');
+      return array('MODULE_BOXES_WISHLIST_STATUS', 'MODULE_BOXES_WISHLIST_CONTENT_PLACEMENT', 'MODULE_BOXES_WISHLIST_SORT_ORDER');
     }
-  }
+}
 ?>
 
